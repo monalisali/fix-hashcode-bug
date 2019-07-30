308cf06e5996b28dfcc81f9c58e4d211509fe1f3
Ben <50296011+MaoYuanFang@users.noreply.github.com>
2019-07-30 17:25:33 +0800

    Update Person.java (#8)
    Update Person.java (#8)

--- src/main/java/com/github/hcsp/collection/Person.java
+++ src/main/java/com/github/hcsp/collection/Person.java
@@ -50,8 +50,7 @@ public class Person {
     @Override
     public int hashCode() {
         int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + age;
+        result = 31 * result + (id != null ? id.hashCode() : 0);
         return result;
     }
 }
