94d5f7c8a64e4ac06c7c42782dd63668ec244a68
王杰 <46479343+wangjie-fourth@users.noreply.github.com>
2019-10-09 13:46:58 +0800

    Set's contains method 是以hahsCode值为依据的 (#37)
    Set's contains method 是以hahsCode值为依据的 (#37)

--- src/main/java/com/github/hcsp/collection/Person.java
+++ src/main/java/com/github/hcsp/collection/Person.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.collection;
 
+import java.util.Objects;
+
 public class Person {
     /** 身份证号 */
     private final String id;
@@ -49,9 +51,6 @@ public class Person {
 
     @Override
     public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + age;
-        return result;
+        return Objects.hash(id);
     }
 }
